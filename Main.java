2cf89b1b54f31741fe215e15356e6b5b2b6945c6
NB1102-hash <NB1102@live.mdx.ac.uk>
2025-04-17 20:50:36 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// This class runs the test and prints the result
+// This class runs the test and prints the result. if the code is successful - prints message Test passed!, if there's an error - prints message Test failed. 
 public class Main {
     public static void main(String[] args) {
         if (Tester.testMergeSort()) {
